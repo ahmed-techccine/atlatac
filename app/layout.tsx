19eb9f430d602bcc864df3c928fd599ffccffbd3
ahmed-techccine <ahmed.techccine@gmail.com>
2025-06-17 14:45:45 -0700

    change meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Atlas Tacs',
+  description: 'Created with v0Atlas TACS is a leading accounting and advisory firm committed to providing exceptional financial services           to businesses and individuals. With years of experience and a client-centric approach, we help you navigate complex financial landscapes and achieve your goals.',
+
 }
 
 export default function RootLayout({
